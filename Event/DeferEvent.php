758d99e131535b9d12223a3fa69941b154d70039
Joakim Friberg <joakim.friberg@gmail.com>
2018-04-10 11:39:07 +0200

    Require symfony v3

--- Event/DeferEvent.php
+++ Event/DeferEvent.php
@@ -14,7 +14,6 @@ class DeferEvent extends Event
             $deferredEvent = new Event();
         }
 
-        $deferredEvent->setName($deferredName);
         $this->deferredEvent = $deferredEvent;
     }
 
